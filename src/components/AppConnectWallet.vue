984bb2d209c86551f6d6000a5c4f8b20b18eb610
Maxim Chernyshov <dsgnrvd@gmail.com>
2023-07-02 03:56:54 +0400

    add stonfi-sdk

--- src/components/AppConnectWallet.vue
+++ src/components/AppConnectWallet.vue
@@ -3,10 +3,12 @@ import { storeToRefs } from 'pinia'
 import { ref, onMounted, nextTick } from 'vue'
 import {TonConnect} from '@tonconnect/sdk'
 import { toUserFriendlyAddress } from '@tonconnect/sdk';
-import { Address } from 'ton3-core';
+import { Address, Coins } from 'ton3-core';
 import QRCodeStyling from './QRCodeStyling.vue';
 import TonWeb from 'tonweb';
 import { ConnectedWalletFromAPI, useWalletStore } from '../stores'
+import { Router, ROUTER_REVISION, ROUTER_REVISION_ADDRESS } from '@ston-fi/sdk';
+import { bytesToBase64, bytesToHex } from 'ton3-core/dist/utils/helpers';
 
 const store = useWalletStore()
 
@@ -46,17 +48,47 @@ const connect = async (wallets: 'tonkeeper' | 'tonhub') => {
 }
 
 const sendTx = async () => {
-  const transaction = {
-      validUntil: Date.now() + 1000000,
-      messages: [
-          {
-            address: store.wallet?.address.raw!,
-            amount: "60000000", 
-          }
-      ]
-  }
-
   try {
+      const WALLET_ADDRESS = store.wallet?.address.bounceable!; // YOUR WALLET ADDRESS
+
+      const REFERRAL_ADDRESS = undefined; // REFERRAL ADDRESS (OPTIONAL)
+
+      const JETTON0 = 'EQBynBO23ywHy_CgarY9NK9FTz0yDsG82PtcbSTQgGoXwiuA'; // JUSDT
+      const JETTON1 = 'EQB-MPwrd1G6WKNkLz_VnV6WqBDd142KMQv-g1O-8QUA3728'; // JUSDC
+
+      const provider = new TonWeb.HttpProvider(undefined, { apiKey: import.meta.env.TON_HTTP_API_KEY });
+
+      const router = new Router(provider, {
+        revision: ROUTER_REVISION.V1,
+        address: ROUTER_REVISION_ADDRESS.V1,
+      });
+
+      // Build transaction params to swap 1e6 JETTON0 to JETTON1
+      // but not less than 1e6 JETTON1
+      const params = await router.buildSwapJettonTxParams({
+        userWalletAddress: WALLET_ADDRESS,
+        offerJettonAddress: JETTON0,
+        askJettonAddress: JETTON1,
+        offerAmount: new TonWeb.utils.BN(1e6),
+        minAskAmount: new TonWeb.utils.BN(1e6),
+        queryId: 12345,
+
+        // Set your address if you want to give referral payouts
+        // from everyone who using this code to swap jettons
+        referralAddress: REFERRAL_ADDRESS,
+      });
+      
+      const transaction = {
+          validUntil: Date.now() + 1000000,
+          messages: [
+              {
+                address: params.to.toString(),
+                amount: params.gasAmount,
+                payload: bytesToBase64(await params.payload.toBoc()),
+              }
+          ]
+      }
+
       const result = await store.entity?.sendTransaction(transaction);
 
       alert('Transaction was sent successfully');
