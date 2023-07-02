cc29c8243cdc59c165aa4d79e3a384609a42b3ab
Maxim <dsgnrvd@gmail.com>
2023-07-02 22:58:08 +0400

    add tds, fix ts issues

--- src/components/QRCodeStyling.vue
+++ src/components/QRCodeStyling.vue
@@ -9,39 +9,21 @@ import { useWalletStore } from '../stores';
 
 const { text } = defineProps<{ text: string }>()
 
-const qrCode = ref(null);
-let qrCodeInstance = null;
+const qrCode = ref<null | HTMLElement>(null);
+let qrCodeInstance: null | QRCodeStyling = null;
 
 const store = useWalletStore();
 
 onMounted(() => {
   qrCodeInstance = new QRCodeStyling({
-    data: text.value,
-    // Other QR code options...
+    data: text,
   });
-  qrCodeInstance.append(qrCode.value);
+  qrCode.value && qrCodeInstance.append(qrCode.value);
 });
 
 store.$subscribe((mutation, state) => {
   if (mutation.storeId === 'wallet' && mutation.type === 'direct') {
-    qrCodeInstance.update({ data: state.connecting.link });
+    qrCodeInstance?.update({ data: state?.connecting?.link });
   }
 });
 </script>
-
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
