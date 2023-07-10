db0a0532b20f8b38bc5730bf03bbe1a119604ab1
Maxim Chernyshov <dsgnrvd@gmail.com>
2023-07-10 17:29:43 +0400

    improve architecture of showing wallet state

--- src/components/AppConnectWallet.vue
+++ src/components/AppConnectWallet.vue
@@ -1,20 +1,15 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, watch } from 'vue'
 import {TonConnect} from '@tonconnect/sdk'
 import QRCodeStyling from './QRCodeStyling.vue';
-import TonWeb from 'tonweb';
 import { ConnectedWalletFromAPI, useWalletStore } from '../stores'
-import { Router, ROUTER_REVISION, ROUTER_REVISION_ADDRESS } from '@ston-fi/sdk';
-import { bytesToBase64, uintToHex } from 'ton3-core/dist/utils/helpers';
-import { Address, BOC, Coins, Slice } from 'ton3-core';
+import { uintToHex } from 'ton3-core/dist/utils/helpers';
+import { Address, BOC, Slice } from 'ton3-core';
 import AppExtraButton from './AppExtraButton.vue'
 import AppModal from './AppModal.vue'
 
 const store = useWalletStore()
-
-const usdToSton = ref('');
-const stonToUsd = ref('');
 
 const walletConnectionSource = {
   tonkeeper: {
@@ -51,70 +46,6 @@ const connect = async (wallets: 'tonkeeper' | 'tonhub') => {
   store.setConnecting({
     link,
   })
-}
-
-const jettons = [
-  { name: 'jusdt', addressMinterBouncable: 'EQBynBO23ywHy_CgarY9NK9FTz0yDsG82PtcbSTQgGoXwiuA', addressMinterRaw: '0:729C13B6DF2C07CBF0A06AB63D34AF454F3D320EC1BCD8FB5C6D24D0806A17C2' },
-  { name: 'ston', addressMinterBouncable: 'EQA2kCVNwVsil2EM2mB0SkXytxCqQjS4mttjDpnXmwG9T6bO', addressMinterRaw: '0:3690254DC15B2297610CDA60744A45F2B710AA4234B89ADB630E99D79B01BD4F' },
-];
-
-const disconnect = async () => {
-  if (store?.entity?.connected) {
-      await store?.entity?.disconnect();
-      store.setWallet(undefined);
-      localStorage.removeItem('wallet');
-  }
-}
-
-const swapJettons = async (leftJetton: string, rightJetton: string, amount: string) => {
-  try {
-      const WALLET_ADDRESS = store.wallet?.address.bounceable!; // YOUR WALLET ADDRESS
-
-      const REFERRAL_ADDRESS = undefined; // REFERRAL ADDRESS (OPTIONAL)
-
-      const JETTON0 = leftJetton;
-      const JETTON1 = rightJetton;
-
-      const provider = new TonWeb.HttpProvider(undefined, { apiKey: import.meta.env.VITE_TON_HTTP_API_KEY });
-
-      const router = new Router(provider, {
-        revision: ROUTER_REVISION.V1,
-        address: ROUTER_REVISION_ADDRESS.V1,
-      });
-
-      // Build transaction params to swap 1e6 JETTON0 to JETTON1
-      // but not less than 1e6 JETTON1
-      const params = await router.buildSwapJettonTxParams({
-        userWalletAddress: WALLET_ADDRESS,
-        offerJettonAddress: JETTON0,
-        askJettonAddress: JETTON1,
-        // 1000000, 1e6 - 1 TON for jUSDT
-        offerAmount: amount,
-        minAskAmount: 0,
-        queryId: 12345,
-
-        // Set your address if you want to give referral payouts
-        // from everyone who using this code to swap jettons
-        referralAddress: REFERRAL_ADDRESS,
-      });
-      
-      const transaction = {
-          validUntil: Date.now() + 1000000,
-          messages: [
-              {
-                address: params.to.toString(),
-                amount: params.gasAmount.add(TonWeb.utils.toNano('0.2')),
-                payload: bytesToBase64(await params.payload.toBoc()),
-              }
-          ]
-      }
-
-      const result = await store.entity?.sendTransaction(transaction);
-
-      console.log('Transaction was sent successfully', result);
-  } catch (e) {
-      console.error(e);
-  }
 }
 
 enum JettonOps {
@@ -269,56 +200,25 @@ watch(() => store?.entity?.connected, async () => {
   }
 })
 
-const { wallet, connecting, loading } = storeToRefs(store)
-
-function isNumber(evt: any) {
-      evt = (evt) ? evt : window.event;
-      var charCode = (evt.which) ? evt.which : evt.keyCode;
-      if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
-        evt.preventDefault();;
-      } else {
-        return true;
-      }
-}
+const { connecting } = storeToRefs(store)
 </script>
 
 <template>
-  <div v-if="loading === false">
-    <div v-if="wallet === undefined" class="p-1">
-      <div class="flex justify-center flex-col">
-        <AppExtraButton text="Connect wallet" :on_click="store.showConnectWalletModalFn" />
-        <AppModal>
-          <template #buttons>
-            <div class="w-[calc(100%-26px-26px)] mx-auto h-[60px] rounded-[20px] bg-[#00000080] cursor-pointer mt-[32px]" @click="connect('tonkeeper')">
-              <span class="text-white-1 flex justify-center items-center h-full">Connect tonkeeper</span>
-            </div>
-            <div class="w-[calc(100%-26px-26px)] mx-auto h-[60px] rounded-[20px] bg-[#00000080] mt-[18px] cursor-pointer" @click="connect('tonhub')">
-              <span class="text-white-1 flex justify-center items-center h-full">Connect tonhub</span>
-            </div>
-            <div class="flex justify-center mt-4">
-              <QRCodeStyling v-if="connecting?.link" :text="connecting.link" />
-            </div>
-          </template>
-        </AppModal>
-      </div>
-    </div>
-    <div v-else class="flex flex-col p-1">
-      <pre class="text-white-1">{{wallet}}</pre>
-      <div class="flex flex-col justify-center mt-2">
-        <input v-model="usdToSton" type="number" @keypress="isNumber" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        <button @click="swapJettons(jettons[0].addressMinterBouncable, jettons[1].addressMinterBouncable, (Number(usdToSton) * 1e6).toString())" class="text-white-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-1 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Swap jettons ({{usdToSton}} jUSDT -> STON)</button>
-      </div>
-      <div class="flex flex-col justify-center mt-2">
-        <input v-model="stonToUsd" type="number" @keypress="isNumber" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        <button @click="swapJettons(jettons[1].addressMinterBouncable, jettons[0].addressMinterBouncable, new Coins(stonToUsd).toNano())" class="text-white-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-1 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Swap jettons ({{stonToUsd}} STON -> jUSDT)</button>
-      </div>
-      <div class="flex justify-center mt-1.5">
-        <button @click="disconnect" class="w-full text-white-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-1 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Disconnect</button>
-      </div>
-    </div>
-  </div>
-  <div v-else class="flex justify-center items-center h-screen">
-    loading wallet state...
+  <div class="flex justify-center flex-col">
+    <AppExtraButton text="Connect wallet" :on_click="store.showConnectWalletModalFn" />
+    <AppModal>
+      <template #buttons>
+        <div class="w-[calc(100%-26px-26px)] mx-auto h-[60px] rounded-[20px] bg-[#00000080] cursor-pointer mt-[32px]" @click="connect('tonkeeper')">
+          <span class="text-white-1 flex justify-center items-center h-full">Connect tonkeeper</span>
+        </div>
+        <div class="w-[calc(100%-26px-26px)] mx-auto h-[60px] rounded-[20px] bg-[#00000080] mt-[18px] cursor-pointer" @click="connect('tonhub')">
+          <span class="text-white-1 flex justify-center items-center h-full">Connect tonhub</span>
+        </div>
+        <div class="flex justify-center mt-4">
+          <QRCodeStyling v-if="connecting?.link" :text="connecting.link" />
+        </div>
+      </template>
+    </AppModal>
   </div>
 </template>
 
