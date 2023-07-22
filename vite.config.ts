f9f874eb510c67e2aef2731c545097c0c4f95fb6
maktd <tima98tima@gmail.com>
2023-07-22 21:31:34 +0300

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), nodePolyfills({
-    // Whether to polyfill `node:` protocol imports.
-    protocolImports: true,
-  }),],
-})
+  base: "/dexf",
+  plugins: [
+    vue(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
+});
